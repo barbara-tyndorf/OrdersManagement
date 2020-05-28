7630a8a6b77bbb62d9a14f02c14a25125f9b4c67
Barbara_Tyndorf <barbaratyndorf@gmail.com>
2020-05-28 22:19:40 +0200

    setup project

---
+++ src/test/java/com/pl/OrdersManagement/OrdersManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pl.OrdersManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
